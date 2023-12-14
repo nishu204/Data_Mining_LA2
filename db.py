4ef642050798f6c399ec3acf2cc9ad1e70846707
Nishad Bagade <85819798+nishu204@users.noreply.github.com>
2023-12-14 18:19:36 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,71 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+from sklearn.metrics import adjusted_rand_score
+from sklearn.preprocessing import StandardScaler
+
+# Load US Census data
+url = r"C:\Users\nisha\Desktop\New folder\us+census+data+1990 (1)\USCensus1990.data.csv"  # Replace with the actual path to your data
+df = pd.read_csv(url)
+
+# Drop irrelevant columns and handle missing values
+df = df.drop(columns=['caseid'])  # Drop 'caseid' column
+df = df.dropna()
+
+# Standardize the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df)
+
+# Apply DBSCAN algorithm
+dbscan = DBSCAN(eps=1, min_samples=5)
+df['cluster'] = dbscan.fit_predict(X_scaled)
+
+# Calculate PCA for visualization
+pca = PCA(n_components=2)
+df_pca = pd.DataFrame(pca.fit_transform(X_scaled), columns=['PC1', 'PC2'])
+df_final = pd.concat([df, df_pca], axis=1)
+
+# Calculate cluster validation accuracy using Adjusted Rand Index
+ari_score = adjusted_rand_score(df['iRspouse'], df['cluster'])
+
+# Dash app initialization
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# App layout
+app.layout = dbc.Container(
+    [
+        html.H1("DBSCAN Clustering on US Census Data"),
+        html.Div(
+            [
+                dcc.Graph(
+                    id='scatter-plot',
+                    figure={
+                        'data': [
+                            {
+                                'x': df_final['PC1'],
+                                'y': df_final['PC2'],
+                                'text': df_final['cluster'],
+                                'mode': 'markers',
+                                'marker': {'color': df_final['cluster']},
+                            }
+                        ],
+                        'layout': {'title': 'DBSCAN Clustering'},
+                    },
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                html.H3(f'Adjusted Rand Index: {ari_score:.2f}'),
+            ]
+        ),
+    ],
+    fluid=True,
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8051)
